9e56aa50bcc0c37f8811eb8cb3658e4525d2c611
William Sukjaem <133365639+Willsu00@users.noreply.github.com>
2024-03-27 14:13:48 +1300

    update hand value

--- HandValue.java
+++ HandValue.java
@@ -1,24 +1,21 @@
-public int handValue() {
-    int value = 0;
-    int aceCount = 0;
+import java.util.List;
 
-    for (Card card : hand) {
-        String rank = card.getRank();
-        if (rank.equals("A")) {
-            value += 11;
-            aceCount++;
-        } else if (rank.matches("K|Q|J")) {
-            value += 10;
-        } else {
-            value += Integer.parseInt(rank);
+public class HandValue {
+    public static int calculate(List<Card> hand) {
+        int value = 0;
+        for (Card card : hand) {
+            String rank = card.getRank();
+            if (rank.equals("A")) {
+                value += 11;
+            } else if (rank.matches("K|Q|J")) {
+                value += 10;
+            } else {
+                value += Integer.parseInt(rank);
+            }
+
         }
+        return value;
     }
 
-    // If value is over 21 and we have aces in hand, reduce value by 10 for each ace
-    while (value > 21 && aceCount > 0) {
-        value -= 10;
-        aceCount--;
-    }
-
-    return value;
+    
 }
